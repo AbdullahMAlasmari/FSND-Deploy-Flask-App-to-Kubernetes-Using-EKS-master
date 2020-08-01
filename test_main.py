7597eaddbac422e08ee1263f49420e053b2bc1c9
Abdullah M. Alasmari <32742632+AbdullahMAlasmari@users.noreply.github.com>
2020-08-01 05:07:13 +0300

    12 commit testing

--- test_main.py
+++ test_main.py
@@ -17,6 +17,7 @@ def client():
     os.environ['JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
+    assert False
 
     yield client
 
@@ -26,15 +27,17 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
